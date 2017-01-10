24fe52d5dd14440a90f04a0da21842f2a2d25ec8
Zhang Haosheng <zhs19951021@163.com>
2017-01-11 00:26:47 +0800

    new file from bxc

---
+++ condition.py
@@ -0,0 +1,45 @@
+from PIL import ImageGrab
+
+class condition():
+    def __init__(self,funScc,funFail,expr):
+        self.fun1=funScc
+        self.fun2=funFail
+        self.expr=expr
+    def execute(self):
+        if (self.expr.boolean()):
+            self.fun1()
+        else :
+            self.fun2()
+class expr():
+    def __init__(self):
+        pass
+    def boolean(self):
+        pass
+    def grab(self):
+        pass
+class dotCmp(expr):
+    def __init__(self,dotPos,color,strict):
+        super().__init__()
+        self.dotPos=dotPos
+        self.color=color
+        self.strict=strict
+    def grab(self):
+        im = ImageGrab.grab((self.dotPos[0],self.dotPos[1],self.dotPos[0]+1,self.dotPos[1]+1))
+        # I am not sure whether the plus one thing above can cause overflow.I think this is right
+        #print (im.size)
+        red, green, blue=im.getpixel((0, 0))
+        return red, green, blue
+    def boolean(self):
+        red, green, blue=self.grab()
+        red, green, blue=self.color[0]-red,self.color[1]-green,self.color[2]-blue
+        if red**2+green**2+blue**2<=self.strict:
+            return True
+        else:
+            return False
+       
+            
+            
+        
+        
+        
+            
---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+
+sys.path.append("C:\\Program Files\\Anaconda3\\Lib\\site-packages")
+from condition import *
+
+def success():
+    print("succeed")
+def failure():
+    print("failed")
+test=condition(success,failure,dotCmp((777,231),(227,185,195),3))
+test.execute()
